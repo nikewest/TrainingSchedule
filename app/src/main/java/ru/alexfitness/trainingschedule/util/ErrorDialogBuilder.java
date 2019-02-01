2e850b04556a8881f833f18c4edc78f6eae51fc1
n.nikiforov <therolexx@mail.ru>
2019-02-01 19:13:43 +0300

    1.5.4.7 some bug fixed

---
+++ app/src/main/java/ru/alexfitness/trainingschedule/util/ErrorDialogBuilder.java
@@ -0,0 +1,28 @@
+package ru.alexfitness.trainingschedule.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import java.net.ConnectException;
+import ru.alexfitness.trainingschedule.R;
+
+public abstract class ErrorDialogBuilder {
+
+    public static void showDialog(Context context, VolleyError error, AlertDialog.OnDismissListener onDismissListener) {
+        String errorText = null;
+        if(error.getCause() instanceof ConnectException){
+            errorText = context.getString(R.string.cant_connect_to_server);
+        } else if(error instanceof TimeoutError){
+            errorText = context.getString(R.string.connection_timeout_exceeded);
+        } else if (error.networkResponse!=null){
+            errorText = new String(error.networkResponse.data);
+        } else errorText = error.toString();
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        dialogBuilder.setMessage(errorText);
+        dialogBuilder.setOnDismissListener(onDismissListener);
+        dialogBuilder.show();
+    }
+
+}
